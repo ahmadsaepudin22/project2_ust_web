7af217a761e0189b74debc1301f7f3cf94727454
ahmadsaepudin22 <149941406+ahmadsaepudin22@users.noreply.github.com>
2023-11-26 11:33:11 +0700

    Add files via upload

---
+++ project2_ust_web/koneksi/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+$database_hostname = "localhost";
+$database_username = "root";
+$database_password = "";
+$database_name = "sttb";
+$database_port = 3306;
+
+try {
+    $database_connection = new PDO("mysql:host=$database_hostname;port=$database_port;dbname=$database_name",
+    $database_username, $database_password);
+    // echo "Koneksi Berhasil";
+} catch (PDOException $x){
+    echo $x->getMessage();
+}
+?>
